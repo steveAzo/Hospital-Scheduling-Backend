675137422eb10d8d958b5841faeed63a0853a7b8
steveAzo -Git <stephenazongo77@gmail.com>
2025-02-13 10:27:25 +0000

    First commit

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+// src/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['patient', 'doctor'], required: true }
+}, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Compare passwords
+UserSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
